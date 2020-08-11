e032087127b6c5ad8e6e404e824ef01080351846
jamesli1997 <jamesli1997@gmail.com>
2020-08-11 12:12:51 -0700

    deleted slides folder

--- assets/Slides/Lec02-Statistics/makezip.sh
+++
@@ -1,3 +0,0 @@
-
-zip -r  Lecture-2-StatisticalModels.zip Lect2-StatModel.Rmd Lect2-StatModel.html Lect2-StatModel_files
-
--- assets/Slides/Lec15-Singlecell/makezip.sh
+++
@@ -1,4 +0,0 @@
-
-zip -r  L15_Single_Cell.zip single_cell_DR.html single_cell_DR.Rmd single_cell_DR_files
-
-
